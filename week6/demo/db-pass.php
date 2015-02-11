901c02c8c5b384f29d644d2ad1364a59eef8061a
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2015-02-11 18:40:11 -0500

    week6

---
+++ week6/demo/db-pass.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+            $passcode = sha1('');
+        
+        
+        ?>
+        
+        <form action="#" method="post">
+            
+           Passcode: <input type="password" value="" />
+            
+            <input type="submit" value="submit" />
+            
+        </form>
+        
+    </body>
+</html>
