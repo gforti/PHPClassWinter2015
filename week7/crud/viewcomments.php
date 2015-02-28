79d18cea05cad4892ab584a96689d3276a0f4421
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2015-02-27 19:30:38 -0500

    week7-friday

--- week7/crud/viewcomments.php
+++ week7/crud/viewcomments.php
@@ -12,16 +12,22 @@
     <body>
         <?php
         // put your code here
-        print_r($comments);
+        //print_r($comments);
         
         if ( count($comments) == 0 ) {
             echo '<p>No comments</p>';
         }
-        echo '<table>';
+        echo '<table border="1">';
+         echo '<tr><th>Name</th><th>email</th><th>comments</th><th>phone</th><th></th><th></th></tr>';
         if ( count($comments) > 0 ) {
             foreach ($comments as $value) {
                 echo '<tr>';
-                echo '<td>',$value , '</td>';
+                echo '<td>',$value['name'] , '</td>';
+                echo '<td>',$value['email'] , '</td>';
+                echo '<td>',$value['comments'] , '</td>';
+                echo '<td>',$value['phone'] , '</td>';
+                echo '<td></td>';
+                echo '<td></td>';
                 echo '</tr>';
             }
         }
