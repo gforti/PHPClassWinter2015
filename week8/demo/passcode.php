0301f9f6df5c1e146df9f03e0bf64e9a859c5fda
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2015-03-04 18:26:32 -0500

    week7-friday

---
+++ week8/demo/passcode.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       
+        ?>
+        
+        <form method="post" action="#">            
+            Passcode <input type="password" value="" name="passcode" />            
+            <input type="submit" value="submit" />          
+        </form>
+    </body>
+</html>
