525d466433035c8f656faca416bc13b26b17b0a1
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2015-03-04 18:22:44 -0500

    week7-friday

--- week8/demo/sessionview.php
+++ week8/demo/sessionview.php
@@ -7,11 +7,24 @@
     <body>
         <?php
             session_start();
-            if ( isset($_SESSION['hello']) ) {
+            
+    if ( isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true ) {
+        echo 'you are logged in';
+    } else {
+        echo 'you are not logged in';
+    }
+            
+            
+            
+            /*
+             if ( isset($_SESSION['hello']) ) {
+             
                   echo $_SESSION['hello'];
             } else {
                 echo 'session not set';
             }
+            
+             */
           
             //echo $_SESSION['cart']['product'];
             //echo $_SESSION['cart']['product1'];
